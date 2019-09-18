e057ec7dd50dbd8d02f13013fa18fa488909b992
Azymic <amylu2009@gmail.com>
2019-09-18 01:43:02 -0400

    removes margin styles and implements script to cycle through identity text

---
+++ TextAnimate.js
@@ -0,0 +1,18 @@
+// Referenced from: https://stackoverflow.com/questions/37242754/how-do-i-change-the-background-image-every-x-seconds
+const identity = [
+  " student", "engineer", "artist"
+]
+
+const node = document.getElementById("identity-text");
+
+const cycleText = (text, container, step) => {
+	text.forEach((text, index) => (
+  	setTimeout(() => {
+	    container.innerHTML = text
+    }, step * (index + 1))
+  ))
+  setTimeout(() => cycleText(text, container, step), step * text.length)
+}
+
+
+cycleText(identity, node, 1500)
